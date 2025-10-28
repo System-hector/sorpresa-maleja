9c7dc967ead841b3f2a443269a42a7db02d53575
Hector Romero <118418875+System-hector@users.noreply.github.com>
2025-10-28 13:20:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-let opened = false;
+const otters = document.querySelectorAll(".otter");
 
-function openBox() {
-    if (opened) return;
-    opened = true;
-
-    document.querySelector('.lid').style.transform = "translateY(-140px) rotate(25deg)";
-    document.querySelector('.gift-box').style.transform = "scale(1.1)";
-    document.getElementById('message').style.display = "block";
-
-    const container = document.getElementById("otters-container");
-    const nutrias = ["🦦", "🦦💞", "💞🦦"];
-
-    nutrias.forEach((emoji, i) => {
-        let otter = document.createElement("div");
-        otter.classList.add("otter");
-        otter.innerHTML = emoji;
-        otter.style.animation = `popUp 1.4s ${i * 0.25}s forwards`;
-
-        container.appendChild(otter);
-    });
-}
+otters.forEach((otter, i) => {
+    otter.style.animation = `popUp 1.3s ${i * 0.25}s forwards`;
+});
